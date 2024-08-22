47774350f1ba646a05613c1a1f86b9aa635a69b8
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-08-22 18:33:33 +0200

    ex02: Remove redondant tests

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 21:59:17 by ibertran          #+#    #+#             */
-/*   Updated: 2024/08/22 17:28:54 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/08/22 18:17:46 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,12 @@ void	test(
 
 		bureaucrat.signForm(shrub);
 		bureaucrat.executeForm(shrub);
-		std::cout << shrub << "\n" << std::endl;
 
 		bureaucrat.signForm(robot);
 		bureaucrat.executeForm(robot);
-		std::cout << robot << "\n" << std::endl;
 
 		bureaucrat.signForm(pres);
 		bureaucrat.executeForm(pres);
-		std::cout << pres << "\n" << std::endl;
 	}
 	catch (std::exception &e) {
 		std::cout << "test failed: " << e.what() << std::endl;
@@ -63,18 +60,4 @@ int main(void)
 	std::cout << "\n" << std::endl;
 
 	test("nanou", "anfichet", 3);
-
-	try
-	{
-		Bureaucrat				me("ibertran", 1);
-		PresidentialPardonForm	form("cdomet-d");
-
-		std::cout 
-			<< me << "\n"
-			<< form << "\n" << std::endl;
-		me.executeForm(form);
-	}
-	catch (std::exception &e) {
-		std::cout << "test failed: " << e.what() << std::endl;
-	}
 }
